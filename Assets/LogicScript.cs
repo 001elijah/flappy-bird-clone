97c5be3e462398a6eeeacfea35fe7f2ce54c225f
illia zolotukha <job.illia.igorevich@gmail.com>
2024-12-20 15:27:45 +0200

    feat: add sound effect

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -7,12 +7,14 @@ public class LogicScript : MonoBehaviour
     public int playerScore;
     public Text scoreText;
     public GameObject gameOverScreen;
+    public AudioSource ding;
 
     [ContextMenu("Increase Score")]
     public void AddScore(int scoreToAdd)
     {
         playerScore += scoreToAdd;
         scoreText.text = playerScore.ToString();
+        ding.Play();
     }
 
     public void RestartGame()
